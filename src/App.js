8e1ebe7ee44554b708b391f1d02d80d023ea8063
Haiyang Hong <hong961127@gmail.com>
2020-11-08 10:39:47 -0800

    Add content

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
-import React from "react";
-
 const App = () => {
   return (
-    <div className="group-1 flex justify-center">
-      <a
-        href="https://github.com/hyhong-code/tailwind-react-starter"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="group-1-hover:bg-teal-100 btn m-4 inline-block bg-white text-primary border-2 border-primary hover:bg-primary hover:text-white transform hover:scale-110 transition ease-out duration-300"
-      >
-        Tailwind with React
-        {/* Icon from https://heroicons.com/ */}
-        <svg
-          className="w-4 inline-block align-middle"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z"
-          />
-        </svg>
-      </a>
+    <div className="container mx-auto">
+      <div className="">
+        <button className="px-8 py-4 border border-gray-800 block mx-auto rounded-full">
+          Open Modal
+        </button>
+      </div>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
+        accusantium similique repudiandae dignissimos, dolorem adipisci, soluta
+        eum cupiditate illo asperiores modi dolorum est officiis labore quia
+        voluptate natus? Laudantium voluptatibus, at incidunt animi, excepturi
+        optio veniam, vel nesciunt officia explicabo pariatur nam ex laborum
+        autem tenetur accusamus ad itaque neque expedita repellat? Ad nisi
+        tenetur iste itaque quidem corrupti eum repudiandae ipsa, sit excepturi
+        at sed voluptates inventore a, earum beatae eligendi qui consequuntur
+        fuga? Aspernatur rem impedit soluta dolores iure! Asperiores provident
+        ab ut impedit similique ipsum error, excepturi enim eaque fuga porro
+        voluptates doloremque doloribus dolorem, iusto officiis velit adipisci
+        soluta amet. Dolore fugiat consequatur a et molestiae pariatur veritatis
+        repellat, quidem voluptate, voluptatum facilis, harum nisi ea porro
+        ipsam accusamus quasi vel reiciendis unde obcaecati. Eveniet magnam nisi
+        facere animi nulla autem sed eum delectus at porro illum ex cumque
+        dolore blanditiis deleniti sequi laborum earum itaque, eaque tempora
+        fuga architecto ipsa quia. Dignissimos impedit suscipit, adipisci
+        recusandae praesentium ab consectetur iusto in pariatur autem minus
+        quidem distinctio at sunt dicta nobis optio, molestias laudantium totam
+        eveniet perspiciatis. Tempore, mollitia? Eius accusantium, at totam
+        saepe molestias, laborum possimus architecto, voluptatibus mollitia
+        numquam nisi dolore itaque molestiae ipsa. Fugiat quo facere beatae
+        similique numquam iure dolores commodi veniam, maxime harum molestiae.
+        Quaerat, cupiditate omnis. Ad aliquam eaque magnam possimus officiis
+        optio quibusdam officia magni! Dolorem officiis dolores explicabo earum
+        illum pariatur quis totam. Aliquid enim sapiente neque consectetur,
+        distinctio cumque possimus. Error eveniet et distinctio commodi iste
+        mollitia ratione itaque animi possimus ea hic, at aut sunt deserunt
+        officia reiciendis optio cum, esse porro placeat inventore odio deleniti
+        suscipit! Nobis aspernatur neque maiores ea voluptatibus voluptatum?
+        Consequatur, adipisci? Culpa at quo, exercitationem provident, iusto ea
+        accusantium ut mollitia vitae eligendi rem explicabo adipisci cumque
+        debitis aliquam repellat totam.
+      </p>
     </div>
   );
 };
